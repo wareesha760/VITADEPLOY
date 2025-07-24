d388407d7d4db3bfbcf4e4ee655bbfcba89e5683
wareesha760 <wareesha.khalid006@gmail.com>
2025-07-25 00:34:26 +0500

    expert

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -3,7 +3,7 @@ import ShopRow from "../components/shprow";
 export default function Home(){
 return (
     <div>
-      
+      {/*  */}
       <h1 className="text-3xl font-bold  text-center pt-30 text-green-700">
         ہماری خدمات
       </h1>
